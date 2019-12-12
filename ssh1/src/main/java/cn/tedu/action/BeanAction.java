9613e0dc85d81bd3295bec6982a036b5dad18bb0
98071 <98071@DESKTOP-3KAKEJR>
2019-12-13 00:29:55 +0800

    1

--- ssh1/src/main/java/cn/tedu/action/BeanAction.java
+++ ssh1/src/main/java/cn/tedu/action/BeanAction.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 @Controller// Bean ID: beanAction
 @Scope("prototype")
 public class BeanAction {	
-	final int a=62222;
+	final int a=622221s;
 	public String execute(){
 		Thread t=Thread.currentThread();
 		System.out.println(t); 
